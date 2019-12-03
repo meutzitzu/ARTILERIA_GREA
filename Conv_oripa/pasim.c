e9893909f5cab8d4335e048168cb43c3291db3aa
meutzitzu <meutzitzu@gmail.com>
2019-12-04 00:20:55 +0200

    added all replace cases

--- Conv_oripa/pasim.c
+++ Conv_oripa/pasim.c
@@ -14,8 +14,15 @@ int main(int argc, char *argv[])
         while(fgets(buff, BUFFER_SIZE - 1, fin) != NULL){
                 printf ("%s", buff);
                 if(strstr(buff, "class=\"mo\"")){
-                        printf("GOTCHA");
+                        printf("MO:GOTCHA\n");
+                } else if(strstr(buff, "class=\"va\"")){
+                        printf("VA:GOTCHA\n");
+                } else if(strstr(buff, "class=\"fl\"")){
+                        printf("FL:GOTCHA\n");
+                } else if(strstr(buff, "class=\"bo\"")){
+                        printf("BO:GOTCHA\n");
                 }
+
         }
 
 
