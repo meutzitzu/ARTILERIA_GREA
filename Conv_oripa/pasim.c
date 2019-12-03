2aa5557e36238bb60cac4b52557dd209bdd6059f
meutzitzu <meutzitzu@gmail.com>
2019-12-04 01:07:31 +0200

    replacing works, but overwrites stuff

--- Conv_oripa/pasim.c
+++ Conv_oripa/pasim.c
@@ -6,12 +6,12 @@
 int main(int argc, char *argv[])
 {
         int n=0;
-        FILE* fin;
-        fin = fopen(argv[1], "r");
+        FILE* fp;
+        fp = fopen(argv[1], "r+");
         printf("\n%s\n", argv[1]);
         char buff[BUFFER_SIZE];
-        //printf("\n\n%d\n", fscanf(fin, "%1023[^\n]s", buff));
-        while(fgets(buff, BUFFER_SIZE - 1, fin) != NULL){
+        //printf("\n\n%d\n", fscanf(fp, "%1023[^\n]s", buff));
+        while(fgets(buff, BUFFER_SIZE - 1, fp) != NULL){
                 printf ("%s", buff);
                 if(strstr(buff, "class=\"mo\"")){
                         printf("MO:GOTCHA\n");
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
                         printf("VA:GOTCHA\n");
                 } else if(strstr(buff, "class=\"fl\"")){
                         printf("FL:GOTCHA\n");
+                        fprintf(fp,"\n\n");
                 } else if(strstr(buff, "class=\"bo\"")){
                         printf("BO:GOTCHA\n");
                 }
